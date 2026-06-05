761c5dad360ac2c8421d293275efb19cf1de1dcb
Astelee <diego.magno321@gmail.com>
2026-06-05 15:29:28 -0300

    Config: Insere chaves reais de produção do Google Cloud e Blogger

--- lib/config/blogger_config.dart
+++ lib/config/blogger_config.dart
@@ -1,9 +1,9 @@
 class BloggerConfig {
   // ID do Blogspot do Horizonte News
-  static const String blogId = 'SEU_BLOG_ID_AQUI';
+  static const String blogId = '2093381478467700202';
   
   // Chave de API gerada no Google Cloud Console com acesso à Blogger API v3
-  static const String apiKey = 'SUA_API_KEY_AQUI';
+  static const String apiKey = 'AIzaSyDkzwRXdsuosdqFIClpTwNuv6Vy-9ZXE9U';
   
   // URL Base para as requisições da API v3
   static const String baseUrl = 'https://www.googleapis.com/blogger/v3/blogs';
