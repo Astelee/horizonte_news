7701e75414c463f8b88e849080af0c265a2b04a4
Diego <88157099+Astelee@users.noreply.github.com>
2026-06-27 10:45:48 -0300

    Update chat_screen.dart

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -4,6 +4,7 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'dart:async';
 import '../config/app_colors.dart';
+import 'package:audioplayers/audioplayers.dart';
 import 'amigos_modelos.dart';
 
 // ═══════════════════════════════════════════════════════════════════
