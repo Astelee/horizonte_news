38d38c6128d689567f1b43793d0c61cc1f31133f
Diego <88157099+Astelee@users.noreply.github.com>
2026-09-02 01:18:25 -0300

    Atualizar o favorites_service.dart

--- lib/services/favorites_service.dart
+++ lib/services/favorites_service.dart
@@ -14,7 +14,9 @@ class FavoritesService {
 
     try {
       final List<dynamic> decodedList = json.decode(favoritesJson);
-      return decodedList.map((item) => PostModel.fromJson(item)).toList();
+      return decodedList
+          .map((item) => PostModel.fromLocalJson(item))
+          .toList();
     } catch (e) {
       // Se houver dados corrompidos ou alteração de formato, reinicializa de forma segura
       return [];
@@ -24,17 +26,8 @@ class FavoritesService {
   /// Salva uma lista inteira de favoritos atualizada no armazenamento local
   Future<void> saveFavorites(List<PostModel> favorites) async {
     final SharedPreferences prefs = await SharedPreferences.getInstance();
-    final List<Map<String, dynamic>> jsonList = favorites.map((post) {
-      return {
-        'id': post.id,
-        'title': post.title,
-        'content': post.content,
-        'url': post.url,
-        'published': post.publishedAt.toIso8601String(),
-        'images': [{'url': post.thumbnailUrl}],
-        'labels': post.categories.map((cat) => cat.name).toList(),
-      };
-    }).toList();
+    final List<Map<String, dynamic>> jsonList =
+        favorites.map((post) => post.toLocalJson()).toList();
 
     await prefs.setString(_favoritesKey, json.encode(jsonList));
   }
