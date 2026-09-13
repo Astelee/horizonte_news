f7c55128d965586c240cb5c8491416147e3e202d
Diego <88157099+Astelee@users.noreply.github.com>
2026-09-13 15:43:39 -0300

    Atualizar o news_tab.dart

--- lib/features/admin/screens/tabs/news_tab.dart
+++ lib/features/admin/screens/tabs/news_tab.dart
@@ -1,5 +1,6 @@
 import 'dart:math' as math;
 import 'dart:ui';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import '../../../../config/app_colors.dart';
 import '../../../../models/post_model.dart';
